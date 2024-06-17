1160a79370846bcdaace52276bc8bffc5e41ba80
ROY <roy@gmail.com>
2024-06-17 12:37:10 +0530

    remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import Button from '@mui/material/Button';
 import SearchBox from './search_box/SearchBox.jsx'
 
 function App() {
-  let handleClick = () => {
-    console.log("button was clicked");
-  };
-
   return (
     <div>
-      <h1>Material UI Demo</h1>
-      <Button variant="contained" onClick={handleClick}>
-        Click me here
-      </Button>
-
-    <SearchBox/>
-
+      <SearchBox/>
     </div>
   );
 }
