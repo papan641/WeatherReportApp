551bba4f3d059d51c597afcfcd8fcafe6596e164
ROY <roy@gmail.com>
2024-06-17 12:43:48 +0530

    all content on center

--- src/search_box/SearchBox.jsx
+++ src/search_box/SearchBox.jsx
@@ -1,17 +1,17 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./SearchBox.css";
-export default function SearchBox(){
-    return(
-        <div className="SearchBox">
+
+export default function SearchBox() {
+    return (
+        <div className="search-box">
             <h3>Search for the weather</h3>
-            <from>
-             <TextField id="city" label="City Name" variant="outlined" required />
-            
-             <Button variant="contained"type="submit">
-                Search
+            <form>
+                <TextField id="city" label="City Name" variant="outlined" required />
+                <Button variant="contained" type="submit">
+                    Search
                 </Button>
-            </from>
-            </div>
+            </form>
+        </div>
     );
 }
